98003ab01797e44aa8c2b7e1c7fbaf9419e4b213
abko <abko121115@gmail.com>
2025-12-09 00:22:35 +0900

    init 초기화

---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <UContainer class="py-10">
+    <UCard>
+      <div class="flex justify-between items-center mb-6">
+        <div>
+          <p class="text-sm opacity-70 mb-1">Welcome to</p>
+          <h1 class="text-2xl font-bold">VBKOROA Nuxt Playground!</h1>
+        </div>
+      </div>
+      <UButton
+        color="primary"
+        @click="count++"
+      >
+        클릭한 횟수: {{ count }}
+      </UButton>
+    </UCard>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+  const count = ref(0)
+</script>
